99dde5f8c75aae8f39dba6b7c20fb71907ca7f61
seya_devJo <seya330@naver.com>
2020-06-15 00:49:37 +0900

    20200615 추가커밋본

--- ranchat/src/main/resources/static/ranchat/js/chat.js
+++ ranchat/src/main/resources/static/ranchat/js/chat.js
@@ -51,12 +51,14 @@ function setConnecting(isConnecting){
 }
 function setConnected(isConnect){
 	if(isConnect){
+		$("#loadingCircle").hide();
 		$("#connectBtn").attr("disabled", true);
 		$("#disConnectBtn").attr("disabled", false);
 		$("#name").attr("disabled", true);
 		$("#sendChatBtn").attr("disabled", false);
 		$("#chatBox").fadeTo(300, 1);
 	}else{
+		$("#loadingCircle").hide();
 		$("#connectBtn").attr("disabled", false);
 		$("#disConnectBtn").attr("disabled", true);
 		$("#name").attr("disabled", false);
@@ -69,7 +71,6 @@ function connectSockAndSubscribe(){
 	var socket = new SockJS("/endPoint");
 	stompClient = Stomp.over(socket);
 	stompClient.connect({chatRoomId: roomId}, function(frame){
-		console.log("connected: " + frame);
 		subscribeMessage();
 		$("#chatBoxContents").html("");
 	});
@@ -77,7 +78,7 @@ function connectSockAndSubscribe(){
 
 function subscribeMessage(){
 	setConnected(true);
-	$("#loadingCircle").hide();
+	
 	stompClient.subscribe('/sub/chat/' + roomId, function(result){
 		var chatMessage = JSON.parse(result.body);
 		
