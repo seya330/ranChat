7c0f40ddd068007a6f552a8390c5dc1e46e7c3b3
조세진 <sejin.jo@wadiz.kr>
2021-06-09 22:26:29 +0900

    first commit

--- ranchat/src/main/java/com/seya330/ranchat/config/WebMvcConfig.java
+++ ranchat/src/main/java/com/seya330/ranchat/config/WebMvcConfig.java
@@ -2,70 +2,47 @@ package com.seya330.ranchat.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mobile.device.DeviceResolverHandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer{
-	
-	//jsp 뷰 리졸버 세팅
-	@Bean
-	public InternalResourceViewResolver viewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setViewClass(org.springframework.web.servlet.view.JstlView.class);
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
-		resolver.setContentType("text/html; charset=UTF-8");
-		resolver.setOrder(1);
-		resolver.setRedirectHttp10Compatible(false);
-		
-		return resolver;
-	}
+public class WebMvcConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		// TODO Auto-generated method stub
-		String jsVer = "1";
-		registry.addResourceHandler("/resources/**")
-		.addResourceLocations("/resources/")
-		.setCachePeriod(-1);
-		
-		//registry.addResourceHandlers(registry);
-	}
-	
-	//spring mobile 인터셉터 세팅
-	@Bean
-	public DeviceResolverHandlerInterceptor deviceResolverHandlerInterceptor() {
-		return new DeviceResolverHandlerInterceptor();
-	}
-	
-	//인터셉터 추가 설정
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(deviceResolverHandlerInterceptor());
-	}
+  @Bean
+  public InternalResourceViewResolver viewResolver() {
+    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+    resolver.setViewClass(org.springframework.web.servlet.view.JstlView.class);
+    resolver.setPrefix("/WEB-INF/views/");
+    resolver.setSuffix(".jsp");
+    resolver.setContentType("text/html; charset=UTF-8");
+    resolver.setOrder(1);
+    resolver.setRedirectHttp10Compatible(false);
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		// TODO Auto-generated method stub
-		registry.addMapping("/**")
-		/*.allowedOrigins("http://localhost:8080", "http://localhost", "http://127.0.0.1:8080", "http://127.0.0.1", "http://ec2-13-125-149-76.ap-northeast-2.compute.amazonaws.com")*/
-		.allowedOrigins("*")
-		.allowedMethods("*")
-		.allowCredentials(false)
-		.maxAge(3600);
-	}
+    return resolver;
+  }
 
-	//DeferredResult 를 사용하는 Async 로직에 타임아웃 60초 설정.
-	@Override
-	public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-		WebMvcConfigurer.super.configureAsyncSupport(configurer);
-		configurer.setDefaultTimeout(30000);
-	}
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/resources/**")
+        .addResourceLocations("/resources/")
+        .setCachePeriod(-1);
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        /*.allowedOrigins("http://localhost:8080", "http://localhost", "http://127.0.0.1:8080", "http://127.0.0.1", "http://ec2-13-125-149-76.ap-northeast-2.compute.amazonaws.com")*/
+        .allowedOrigins("*")
+        .allowedMethods("*")
+        .allowCredentials(false)
+        .maxAge(3600);
+  }
+
+  //DeferredResult 를 사용하는 Async 로직에 타임아웃 60초 설정.
+  @Override
+  public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+    WebMvcConfigurer.super.configureAsyncSupport(configurer);
+    configurer.setDefaultTimeout(30000);
+  }
 }
