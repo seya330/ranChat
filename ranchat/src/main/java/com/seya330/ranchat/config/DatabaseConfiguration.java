0399b11dc812e3cb8bf83f993e825423846da40b
조세진 <sejin.jo@wadiz.kr>
2021-06-09 23:32:15 +0900

    yml파일 프로파일 설정, 데이터소스 설정

--- ranchat/src/main/java/com/seya330/ranchat/config/DatabaseConfiguration.java
+++ ranchat/src/main/java/com/seya330/ranchat/config/DatabaseConfiguration.java
@@ -1,16 +1,13 @@
 package com.seya330.ranchat.config;
 
-import java.io.IOException;
-import java.util.Properties;
-
-import javax.sql.DataSource;
-
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,58 +16,41 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
-//@MapperScan(value= {""}, sqlSessionFactoryRef="")
 public class DatabaseConfiguration {
-	@Value("${spring.datasource.driver}")
-	String driverClassName;
-	
-	@Value("${spring.datasource.url}")
-	String jdbcUrl;
-	
-	@Value("${spring.datasource.username}")
-	String userName;
-	
-	@Value("${spring.datasource.password}")
-	String password;
-	
-	@Bean
-	public DataSource dataSource() {
-		HikariConfig conf = new HikariConfig();
-		//conf.setDriverClassName("org.mariadb.jdbc.Driver");
-		conf.setDriverClassName(driverClassName);
-		conf.setJdbcUrl(jdbcUrl);
-		conf.setUsername(userName);
-		conf.setPassword(password);
-		return new HikariDataSource(conf);
-	}
-	
-	@Primary
-	@Bean(name="defaultFactory")
-	public SqlSessionFactory defaultFactory(ApplicationContext context) throws Exception{
-		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-		factoryBean.setDataSource(dataSource());
-		factoryBean.setMapperLocations(context.getResources("classpath:mapper/com/seya330/ranchat/**/*.xml"));
-		
-		/*
-		 * 작동이안됨 Properties properties = new Properties();
-		 * properties.setProperty("mapUnderscoreToCamelCase", "true");
-		 * factoryBean.setConfigurationProperties(properties);
-		 */
-		
-		Resource resource = new PathMatchingResourcePatternResolver().getResource("classpath:mybatis-config.xml"); 
-		factoryBean.setConfigLocation(resource);
-		
-		return factoryBean.getObject();
-	}
-	
-	@Primary
-	@Bean(name="defaultSqlSession")
-	public SqlSessionTemplate defaultSqlSession(@Qualifier("defaultFactory") SqlSessionFactory sqlSessionFactory) {
-		return new SqlSessionTemplate(sqlSessionFactory);
-	}
+
+  @Bean
+  @ConfigurationProperties("spring.datasource")
+  public DataSourceProperties dataSourceProperties() {
+    return new DataSourceProperties();
+  }
+
+  @Bean
+  public DataSource dataSource(final DataSourceProperties dataSourceProperties) {
+    return dataSourceProperties.initializeDataSourceBuilder()
+        .type(HikariDataSource.class)
+        .build();
+  }
+
+  @Primary
+  @Bean(name = "defaultFactory")
+  public SqlSessionFactory defaultFactory(ApplicationContext context, @Autowired DataSource dataSource) throws Exception {
+    SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+    factoryBean.setDataSource(dataSource);
+    factoryBean.setMapperLocations(context.getResources("classpath:mapper/com/seya330/ranchat/**/*.xml"));
+
+    Resource resource = new PathMatchingResourcePatternResolver().getResource("classpath:mybatis-config.xml");
+    factoryBean.setConfigLocation(resource);
+
+    return factoryBean.getObject();
+  }
+
+  @Primary
+  @Bean(name = "defaultSqlSession")
+  public SqlSessionTemplate defaultSqlSession(@Qualifier("defaultFactory") SqlSessionFactory sqlSessionFactory) {
+    return new SqlSessionTemplate(sqlSessionFactory);
+  }
 }
