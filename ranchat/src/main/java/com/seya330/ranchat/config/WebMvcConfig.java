ede6862187720d5f14fcd896b9389c99578fd151
조세진 <sejin.jo@wadiz.kr>
2021-06-11 15:11:50 +0900

    redis, mysql 로컬에서 작동하도록 수정

--- ranchat/src/main/java/com/seya330/ranchat/config/WebMvcConfig.java
+++ ranchat/src/main/java/com/seya330/ranchat/config/WebMvcConfig.java
@@ -31,11 +31,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-        /*.allowedOrigins("http://localhost:8080", "http://localhost", "http://127.0.0.1:8080", "http://127.0.0.1", "http://ec2-13-125-149-76.ap-northeast-2.compute.amazonaws.com")*/
-        .allowedOrigins("*")
-        .allowedMethods("*")
+    registry
+        .addMapping("/**")
         .allowCredentials(false)
+        .allowedOriginPatterns("*")
+        .allowedMethods("*")
         .maxAge(3600);
   }
 
---
+++ ranchat/src/test/java/com/seya330/ranchat/SampleTest.java
@@ -0,0 +1,24 @@
+package com.seya330.ranchat;
+
+import com.seya330.ranchat.user.vo.RegUserVO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+@SpringBootTest
+public class SampleTest {
+
+  @Autowired
+  private RedisTemplate redisTemplate;
+
+  @Test
+  void name() {
+    ValueOperations<String, RegUserVO> redisLoginOperation = redisTemplate.opsForValue();
+    RegUserVO vo = new RegUserVO();
+    vo.setUserId("1345");
+    vo.setPassword("asdfasdf");
+    redisLoginOperation.set("12345", vo);
+  }
+}
